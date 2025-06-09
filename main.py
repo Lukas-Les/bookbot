98a3635ec1ac785b435d3f26cdad74ac902b96a5
stabmeldys <lukas.les93@gmail.com>
2025-06-09 19:59:30 +0300

    finalized bot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words, get_num_symbols, generate_report
 
 
 def get_book_text(path_to_file: str) -> str:
@@ -7,7 +8,11 @@ def get_book_text(path_to_file: str) -> str:
     return file_contents
 
 def main() -> None:
-    book_text = get_book_text("books/frankenstein.txt")
-    get_num_words(book_text)
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    book_text = get_book_text(file_path)
+    print(generate_report(file_path, book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,33 @@
-def get_num_words(text: str) -> None:
+from collections import defaultdict
+
+
+def get_num_words(text: str) -> int:
     num_words = len(text.split())
-    print(f"{num_words} words found in the document")
-    
+    return num_words
+
+
+def get_num_symbols(text: str) -> dict:
+    text_split = text.lower().split()
+    result = defaultdict(int)
+    for word in text_split:
+        for s in word:
+            result[s] +=1
+    return result
+
+
+def generate_report(file_path: str, book_text: str) -> str:
+    words_total = get_num_words(book_text)
+    message = f"""
+============ BOOKBOT ============
+Analyzing book found at {file_path}...
+----------- Word Count ----------j
+Found {words_total} total words
+--------- Character Count -------\n
+"""
+    symmbol_count = get_num_symbols(book_text)
+    symmbol_count_list = [{k: v} for k, v in symmbol_count.items() if k.isalpha()]
+    symmbol_count_list.sort(key=lambda x: list(x.values()), reverse=True)
+    for item in symmbol_count_list:
+        message += f"{list(item.keys())[0]}: {list(item.values())[0]}\n"
+    message += "============= END ==============="
+    return message
